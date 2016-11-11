c8b15d3f191406e06d33529a9647991ae2c630bc
trierra <trierra.dev@gmail.com>
2016-11-10 16:01:26 -0800

    docs added. Refactored

--- src/main/java/app/service/serviceUtils/BreadthFirstSearch.java
+++ src/main/java/app/service/serviceUtils/BreadthFirstSearch.java
@@ -4,50 +4,60 @@ import app.storage.GraphDB;
 
 import java.util.Stack;
 
+/**
+ * Graph processing algorithm for searching of shortest path between users
+ */
 public class BreadthFirstSearch {
     private static final int INFINITY = Integer.MAX_VALUE;
-    private boolean[] marked;  // marked[v] = is there an s-v path
-    private int[] edgeTo;      // edgeTo[v] = previous edge on shortest s-v path
-    private int[] distTo;      // distTo[v] = number of edges shortest s-v path
+    private boolean[] marked;  // marked[v] = is there an source-vertex path
+    private int[] edgeTo;      // edgeTo[v] = previous edge on shortest source-vertex path
+    private int[] distTo;      // distTo[v] = number of edges shortest source-vertex path
 
-    public BreadthFirstSearch(GraphDB G, int s) {
+    public BreadthFirstSearch(GraphDB G, int source) {
         marked = new boolean[G.getVertexes()];
         distTo = new int[G.getVertexes()];
         edgeTo = new int[G.getVertexes()];
-        bfs(G, s);
+        bfs(G, source);
     }
 
-
-    // breadth-first search from a single source
-    private void bfs(GraphDB G, int s) {
+    /**
+     * breadth-first search from a single source
+     */
+    private void bfs(GraphDB G, int source) {
         Queue<Integer> q = new Queue<Integer>();
         for (int v = 0; v < G.getVertexes(); v++)
             distTo[v] = INFINITY;
-        distTo[s] = 0;
-        marked[s] = true;
-        q.enqueue(s);
+        distTo[source] = 0;
+        marked[source] = true;
+        q.enqueue(source);
 
         while (!q.isEmpty()) {
-            int v = q.dequeue();
-            for (int w : G.getAdjacencyList(v)) {
-                if (!marked[w]) {
-                    edgeTo[w] = v;
-                    distTo[w] = distTo[v] + 1;
-                    marked[w] = true;
-                    q.enqueue(w);
+            int vert1 = q.dequeue();
+            for (int vert2 : G.getAdjacencyList(vert1)) {
+                if (!marked[vert2]) {
+                    edgeTo[vert2] = vert1;
+                    distTo[vert2] = distTo[vert1] + 1;
+                    marked[vert2] = true;
+                    q.enqueue(vert2);
                 }
             }
         }
     }
 
-    public boolean hasPathTo(int v) {
-        return marked[v];
+    boolean hasPathTo(int vertex) {
+        return marked[vertex];
     }
 
-    public int distTo(int v) {
-        return distTo[v];
+    public int distTo(int vertex) {
+        return distTo[vertex];
     }
 
+    /**
+     * Returns path to particular vertex
+     *
+     * @param v - vertex
+     * @return
+     */
     public Iterable<Integer> pathTo(int v) {
         if (!hasPathTo(v)) return null;
         Stack<Integer> path = new Stack<Integer>();
